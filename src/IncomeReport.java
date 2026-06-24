5a27b249eef50936373d07fa46c641ad6abbbdc1
Blake Feehan <feehanb@ferris.edu>
2026-06-24 03:16:32 -0400

    income report logic demo

---
+++ src/IncomeReport.java
@@ -0,0 +1,27 @@
+
+public class IncomeReport {
+	double[] incomeAmounts = {2000, 1500, 500};
+	String[] months = {"January", "Febuary", "Febuary"};
+	String[] sourseIncome = {"Job", "Job", "Gift"};
+	int i;
+	
+	
+	public IncomeReport() {
+	double total = 0;
+	
+	System.out.println("Report\n");
+	
+	for (i = 0; i < incomeAmounts.length; ++i) {
+		System.out.println("Amount: " + incomeAmounts[i] + ", Month: " + months[i] + ", Source: " + sourseIncome[i]);
+		total += incomeAmounts[i];
+		
+	}
+	
+System.out.println("Total: " + total); 
+	}
+	public static void main(String[] args) {
+		new IncomeReport();
+
+}
+
+}
