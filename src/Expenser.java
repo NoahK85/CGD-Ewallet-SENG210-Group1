8bce8185dac89349ccc7b58c8b28f2c80f2982aa
NoahKabelman <kabelmn@ferris.edu>
2026-06-26 19:17:41 -0400

    Features from previous pull requests, addExpense, addIncome, and updateMonthlySavings

--- src/Expenser.java
+++ src/Expenser.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 public interface Expenser {
 public User userAtHand= null;
 	// As a user I'd like to add a monthly expense so I can track and report my expenses - 3pts
-	public void addExpense (Expense Ex); 
+	public void addExpense (User u, Expense Ex); 
 	// As a user I'd like to add a monthly income so I can track and report my income all year - 3pts
-	public void addMonthlyIncome (Wage W);
+	public void addMonthlyIncome (User u, Wage W);
 	//As  a user I would like to view a detailed report of all expenses, income, and summary information 
 	//summary information include : total income, total income for each type, total income for each month, total expense, total expense for each type, 
 	//total savings (total income- total expenses) to date, if the total savings are less than zero it should be reported as total new debt. 	
@@ -15,7 +15,7 @@ public User userAtHand= null;
 	//As  a user I would like to view a detailed report of all income, and summary information for income
 	public void PrintIncomereport();
 	//As  a user I would like to view a detailed report of income of a certain type, and summary information for income
-	public void PrintIncomereportbyTpe();
+	public void PrintIncomereportbyType();
 	//As  a user I would like to view a detailed report of expense of a certain type , and summary information for expenses
 	public void PrintExpensebyType();
 	// As a user I would like to choose a report and export it as an external file (any type is fine preferences are csv or JSON)
@@ -30,7 +30,7 @@ public User userAtHand= null;
 	// As a user I would like to provide an item and a price and get an estimate in number of months needed to save up to buy this item. (based on current monthly saving. 
 	public int whenCanIBuy(String itemname,double  price);
 	// updates monthly savings based on latest added income and expenses. This is an internal function not called by the users.  Bonus: what is the most efficient way to call it (when?)? 
-	public void updateMonthlySavings(); 
+	public void updateMonthlySavings(User user); 
 	
 	
 }
--- src/User.java
+++ src/User.java
@@ -11,5 +11,33 @@ public class User {
 	// possible monthly savings, calculated using monthly income (most recent) assuming the data we have is for one year, and monthly and biweekly expenses, here you can assume yearly expenses that are recorded have already been paid. 
 	double monthlysavings;	
 	//should add constructor(s)
-	User(String username,String password){}
+	User(String username,String password){
+		this.username = username;
+		this.pwd      = password;
+	}
+	
+	//get the most recently input wage
+	public Wage getRecentWage() {
+		if (Income.size() > 0) {
+			return Income.get(0);
+		}
+		else {
+			return new Wage("", 0.0, "");
+		}
+	}
+	
+	//get the list of expenses
+	public ArrayList<Expense> getExpenses() {
+		return Spending;
+	}
+	
+	//adds wage to incomes
+	public void addWage(Wage w) {
+		Income.add(0, w);
+	}
+	
+	//adds expense to spending
+	public void addExpense(Expense e) {
+		Spending.add(e);
+	}
 }
